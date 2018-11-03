d5d98c08e5c36534c00d5c42aec2f9cc4309f8d5
Jerome <raoju1580@qq.com>
2018-11-03 11:34:01 +0800

    工厂模式

---
+++ src/index_factory.js
@@ -0,0 +1,22 @@
+class Product{
+    constructor(name){
+        this.name = name
+    }
+    init(){
+        alert('init')
+    }
+    fn1(){
+        alert('fn1')
+    }
+}
+
+class Creator{
+    create(name){
+        return new Product(name)
+    }
+}
+
+let creator = new Creator()
+let p = creator.create('p1')
+p.init()
+p.fn1()
